0c54e485b59c8ad03e5e91d62761776464de65ae
Shubham Mishra <shubham.smishra2004@gmail.com>
2026-06-02 22:23:32 +0530

    added model evaluation artifact

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -34,3 +34,10 @@ class ModelEvaluationArtifact:
     changed_accuracy:float
     s3_model_path:str 
     trained_model_path:str
+
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted:bool
+    changed_accuracy:float
+    s3_model_path:str 
+    trained_model_path:str
